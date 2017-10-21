8083a14a5612db2cee5a3f2ae5eb2be0cbddb8a5
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-10-21 16:35:59 +0330

    feat: add Container

---
+++ components/Container.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+import rem from 'utils/rem'
+
+const Container = styled.div`
+  max-width: ${rem(1140)};
+  padding: 0 ${rem(10)};
+  margin: 0 auto;
+  box-sizing: content-box;
+`
