4130d696708639c5a28fcc14fb39c4e9bb7a57bb
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-10-21 19:45:09 +0330

    feat: add multiple spacing options to section title

--- components/SectionTitle.js
+++ components/SectionTitle.js
@@ -3,8 +3,14 @@ import styled from 'styled-components'
 import { specialRed } from 'utils/colors'
 import rem from 'utils/rem'
 
+const spacingTypes = {
+  'normal': 16,
+  'medium': 35,
+  'large': 45,
+}
+
 const SectionTitle = styled.h1`
-  margin-bottom: ${rem(16)};
+  margin-bottom: ${p => rem(spacingTypes[ p.spacing || 'normal' ])};
 
   line-height: ${rem(19)};
   font-size: ${rem(16)};
