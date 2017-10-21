db84423aff10fd81151e75c9399e74f8300851c9
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-10-21 19:47:09 +0330

    feat: add sections to index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import JoinUsIntro from '../sections/JoinUsIntro'
 import JoinTheCommunity from '../sections/JoinTheCommunity'
+import Highlights from '../sections/Highlights'
 
 export default () => (
   <div>
     <JoinUsIntro />
     <JoinTheCommunity />
+    <Highlights />
   </div>
 )
