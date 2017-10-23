c846e0a467ec58542903fa5ca25e6d84427ee23d
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-10-23 20:18:29 +0330

    feat: add helmet to server to tick some security boxes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const next = require('next')
 const path = require('path')
+const next = require('next')
+const express = require('express')
+const helmet = require('helmet')
 
 const port = parseInt(process.env.PORT, 10) || 4004
 const dev = process.env.NODE_ENV !== 'production'
@@ -10,6 +11,11 @@ const handle = app.getRequestHandler()
 app.prepare()
   .then(() => {
     const server = express()
+
+    // Obvious security boxes
+    server.use(helmet())
+    server.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
+    server.use(helmet.referrerPolicy({ policy: 'same-origin' }));
 
     // serve service worker
     server.get('/sw.js', (req, res) => res.sendFile(path.resolve('./.next/sw.js')));
