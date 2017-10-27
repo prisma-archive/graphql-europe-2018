bd714302e854177c96ad7af9a21ef89ba59bb3b6
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-10-27 12:05:48 +0330

    fix: change social banner to the pink one

--- pages/_document.js
+++ pages/_document.js
@@ -57,14 +57,14 @@ export default class MyDocument extends Document {
           <meta property="og:url" content="https://graphql-europe.org/"/>
           <meta property="og:title" content="GraphQL-Europe - Berlin, June 15th, 2018 - Join Europe’s biggest GraphQL-dedicated conference"/>
           <meta property="og:description" content="GraphQL-Europe - Berlin, June 15th, 2018 - Join Europe’s biggest GraphQL-dedicated conference"/>
-          <meta property="og:image:url" content="https://graphql-europe.org/static/GrapQL-Europe-2018-banner.png"/>
+          <meta property="og:image:url" content="https://graphql-europe.org/static/GrapQL-Europe-2018-banner.jpg"/>
 
           <meta name="twitter:card" content="summary_large_image"/>
           <meta name="twitter:site" content="@graphqleu"/>
           <meta name="twitter:creator" content="@graphqleu"/>
           <meta name="twitter:title" content="GraphQL-Europe - Join Europe’s biggest GraphQL-dedicated conference"/>
           <meta name="twitter:description" content="GraphQL-Europe - Berlin, June 15th, 2018 - Join Europe’s biggest GraphQL-dedicated conference"/>
-          <meta name="twitter:image" content="https://graphql-europe.org/static/GrapQL-Europe-2018-banner.png"/>
+          <meta name="twitter:image" content="https://graphql-europe.org/static/GrapQL-Europe-2018-banner.jpg"/>
 
           <link rel="canonical" href="https://graphql-europe.org" />
 
