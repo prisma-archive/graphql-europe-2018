258e2a2ae0d37118aecf7b17fc02840777835674
Nikolas Burk <nikolas.burk@gmail.com>
2018-05-17 09:07:00 +0200

    update speaker and schedule data

--- sections/Schedule.js
+++ sections/Schedule.js
@@ -49,6 +49,7 @@ const scheduleList = [
   },
   {
     title: 'Keynote',
+    abstract: 'TBA',
     description: 'Nick Schrock',
     images: [
       {
@@ -56,10 +57,12 @@ const scheduleList = [
       },
     ],
     time: '09:40',
-    ...styles.keynote,
+    ...styles.regular,
   },
   {
     title: ' Case Study: Scaling GraphQL at Shopify',
+    abstract:
+      'In 2016, Shopify began using GraphQL in production. The following year, we launched our first public GraphQL API. Now, we are rapidly scaling our GraphQL APIs both internally and externally changing engineering processes and teams along the way. This is a story about how we scale GraphQL at Shopify.',
     description: 'Leanne Shapton',
     images: [
       {
@@ -71,6 +74,7 @@ const scheduleList = [
   },
   {
     title: 'Breaking up the monolith – A layered GraphQL architecture',
+    abstract: 'TBA',
     description: 'Johannes Schickling',
     images: [
       {
@@ -82,6 +86,7 @@ const scheduleList = [
   },
   {
     title: 'Coffee break (30 min)',
+    abstract: '',
     description: 'Talk about GraphQL while enjoying fresh coffee and tea',
     images: [
       {
@@ -94,10 +99,12 @@ const scheduleList = [
   },
   {
     title: 'Transpiling GraphQL instead of writing customized server code',
+    abstract:
+      'GraphQL specifies what data and response shape we need and not how to get and reshape that data. At Twitter, we automatically translate GraphQL queries into code that efficiently specifies the how as well! See how we keep our API uniform and extend it without writing or deploying new code.',
     description: 'Mike Solomon',
     images: [
       {
-        src: [],
+        src: ['/static/speakers/mike-solomon.jpg'],
       },
     ],
     time: '11:10',
@@ -105,6 +112,8 @@ const scheduleList = [
   },
   {
     title: 'Fundamental Properties of the GraphQL Language',
+    abstract:
+      'This talk presents a formal study of the GraphQL language. After a gentle introduction to the typical problems considered in such studies, I highlight our findings regarding these problems for the case of GraphQL. As a bonus, I present a solution to avoid producing overly large query results.',
     description: 'Olaf Hartig',
     images: [
       {
@@ -115,7 +124,8 @@ const scheduleList = [
     ...styles.regular,
   },
   {
-    title: 'GraphQL Recipe',
+    title: ' Making the Case for GraphQL: A Recipe',
+    abstract: 'TBA',
     description: 'Jira Vinyoopongphan',
     images: [
       {
@@ -127,6 +137,8 @@ const scheduleList = [
   },
   {
     title: 'Supercharge Your GraphQL Development',
+    abstract:
+      'Supercharge your GraphQL development with the linting, formatting, and static analysis tools you need to write cleaner and more reliable GraphQL.',
     description: 'Jon Wong',
     images: [
       {
@@ -150,6 +162,8 @@ const scheduleList = [
   },
   {
     title: 'Going Full Graph',
+    abstract:
+      'At Docker, we began using GraphQL in an unconventional way -- without a server. Using a client-side schema we gained major benefits from GraphQL with a smaller buy-in. Today we are moving our GraphQL interface to the server and exploring new technologies like GraphQL bindings, ReasonML, and more.',
     description: 'Tyler Martinez',
     images: [
       {
@@ -161,6 +175,8 @@ const scheduleList = [
   },
   {
     title: '2 Fast 2 Furious: migrating Medium’s codebase without slowing down',
+    abstract:
+      "After 5 years, we're building the next generation infrastructure at Medium with GraphQL and we're doing it without slowing product development and we're incrementally gaining benefits from the new system. See how we take advantage of GraphQL to enable widespread yet gradual architectural change!",
     description: 'Sasha Solomon',
     images: [
       {
@@ -173,6 +189,8 @@ const scheduleList = [
   {
     title:
       "Not your Dad's Java - Building a GraphQL Server with Kotlin and graphql-java",
+    abstract:
+      'Building APIs in Java is not exciting. That changes if you throw GraphQL and Kotlin into the mix. The great library (thanks Andy) and powerful, concise language make it easy to build a GraphQL server in a few lines of code. With co-routines and lambdas as a glue, you get a neat, scalable backend.',
     description: 'Michael Hunger',
     images: [
       {
@@ -184,10 +202,12 @@ const scheduleList = [
   },
   {
     title: 'Write your own GraphQL Client ',
-    description: 'Rob Richards',
+    abstract:
+      'Have you ever asked "why do I need a library like Apollo or Relay to use GraphQL?" They can seem complex and intimidating. In this talk we will build our own client from the ground up to get a deep understanding of the advanced features these libraries offer.',
+    description: 'Rob Richard',
     images: [
       {
-        src: ['/static/speakers/rob-richards.jpg'],
+        src: ['/static/speakers/rob-richard.jpg'],
       },
     ],
     time: '14:20',
@@ -207,6 +227,8 @@ const scheduleList = [
   },
   {
     title: 'Supercharged SDL',
+    abstract:
+      "How we used SDL, custom directives and a little expression language to unlock the GraphQL flexibility we needed. Bringing the power of GraphQL to XING is an ongoing challenge and we'd like to show you some of our answers to the most pressing question we faced down that road.",
     description: 'David Krentzlin',
     images: [
       {
@@ -218,6 +240,8 @@ const scheduleList = [
   },
   {
     title: 'Teaching GraphQL',
+    abstract:
+      "GraphQL is designed to be easy to use, but newcomers to GraphQL are often tripped up by common problems and misconceptions. I'll cover how to teach GraphQL in a way that gets newbies excited ands helps them overcome the mental hurdles that prevent them from being productive with GraphQL.",
     description: 'Daniel Woelfel',
     images: [
       {
@@ -230,6 +254,8 @@ const scheduleList = [
   {
     title:
       'GraphQL Subscriptions in their untold glory: building a public Webhook API',
+    abstract:
+      "If you ever used or design a webhook (aka \"events\") API, you must have noticed that every one of them is a (usually messy) unique snowflake. Using GraphQL Subscriptions is the solution to fix all that, we've done it and it's much simpler than you'd think!",
     description: 'Arnaud Rinquin',
     images: [
       {
@@ -241,6 +267,8 @@ const scheduleList = [
   },
   {
     title: 'GraphQL + Apollo + Vue = Magic',
+    abstract:
+      "We all here love GraphQL and we have seen how amazing Apollo can be in combination with React. But what about Vue? Vue is by itself reactive and that makes it so perfect for GraphQL and Apollo. Let's look at how to use this awesome combination to make awesomer things.",
     description: 'Sara Vieira',
     images: [
       {
@@ -264,6 +292,7 @@ const scheduleList = [
   },
   {
     title: 'TBA',
+    abstract: 'TBA',
     description: 'Peggy Rayzis',
     images: [
       {
@@ -275,6 +304,8 @@ const scheduleList = [
   },
   {
     title: 'GraphQL + Live Queries: 2 Years in Production',
+    abstract:
+      "At Samsara, we've used live queries in production for the last two years to render live data in our applications by default with minimal boilerplate and plumbing. We'll discuss our experiences: reactive backends, developer happiness, and how you too can add live queries to an existing system.",
     description: 'Stephen Wan',
     images: [
       {
@@ -286,6 +317,8 @@ const scheduleList = [
   },
   {
     title: "Fast, Slow, Beautiful, Ugly: Building GitHub's Platform",
+    abstract:
+      "GitHub has had a REST API for almost 10 years and it's powered hundreds of thousands of integrations. In the last 2 years, GitHub has been reinvesting in its platform and brought a new, public GraphQL API to market. Additionally, GitHub's engineers build, maintain, and use their own GraphQL APIs to power GitHub.com itself. In this talk, Kyle will share why GitHub is enabling integrators to build more powerful integrations, why GraphQL is an important part of the strategy, and all the joy, tears, bad code, and amazing accomplishments of our journey.",
     description: 'Kyle Daigle',
     images: [
       {
