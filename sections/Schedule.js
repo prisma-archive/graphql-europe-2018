30ac03485e897c5f1fd67393b9664a9c1aa4123d
Nikolas Burk <nikolas.burk@gmail.com>
2018-06-05 09:01:47 +0200

    add nick's keynote description

--- sections/Schedule.js
+++ sections/Schedule.js
@@ -48,8 +48,10 @@ const scheduleList = [
     ...styles.keynote,
   },
   {
-    title: 'Keynote',
-    abstract: 'TBA',
+    title:
+      'The Revenge of the Monolith: The Past, Present, and Future of GraphQL-Native',
+    abstract:
+      'GraphQL was open sourced three years ago, and released within the context of a client/front-end ecosystem: React. GraphQL has strong adoption and momentum within these front-end communities. GraphQL was also released into a world moving quickly towards fine-grained microservices. As a result, most GraphQL server-side implementations are thin integration layers over REST APIs or microservices that require minimal changes to existing backend architectures. This talk will argue that is time to make more of our backend systems “GraphQL-Native” and that this approach can reduce the architectural and conceptual costs of adopting a fine-grained microservices architecture.',
     description: 'Nick Schrock',
     images: [
       {
